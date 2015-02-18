4ac88d78a3cf2d11689b57c0e9460361d76afa69
Michael DeLucco <mdelucco@gmail.com>
2015-02-17 19:07:12 -0800

    (deploy) added yml file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
+var user = process.env.mongo_lab_user;
+var password = process.env.mongo_lab_password;
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/flockdocs'); // connect to mongo database named flockdocs
+mongoose.connect('mongodb://'+user+':'+password+'@ds045531.mongolab.com:45531/flockdocs'); // connect to mongo database named flockdocs
 
 // configure our server with all the middleware and and routing
 require('./middleware.js')(app, express);
