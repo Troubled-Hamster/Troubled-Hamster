29c0118133e3774b7f139a39897d5617bb79a6d4
Shin Uesugi <shinsaku417@gmail.com>
2015-02-19 16:06:44 -0800

    (fix) fixed file path for annotation

--- client/annotate.js
+++ client/annotate.js
@@ -1,6 +1,6 @@
 $(function() {
   var content = $('#content').annotator();
-  content.annotator('setupPlugins', {tokenUrl: 'http://localhost:3000/api/token'}, {
+  content.annotator('setupPlugins', {tokenUrl: 'http://flockdocs-dev.elasticbeanstalk.com/api/token'}, {
              Tags: false,
              Filter: false
            });
